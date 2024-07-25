7d213906786b3dcd6f24548b4a4f7193f2c252d6
Lui Habl <txaicalvoso@gmail.com>
2024-07-25 20:18:02 -0300

    Add constant headers

---
+++ include/kn/constants/constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace kn::constants {
+
+    // Elementary charge [C]
+    constexpr double e = 1.602176634e-19;
+
+    // Electron mass [kg]
+    constexpr double m_e = 9.1093837139e-31;
+    
+}
