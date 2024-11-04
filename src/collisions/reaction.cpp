39a0233ffec136a5fe1ae00fdc75629ba140740e
Lui Habl <txaicalvoso@gmail.com>
2024-11-04 19:59:18 -0300

    Refactor

--- src/collisions/reaction.cpp
+++ src/collisions/reaction.cpp
@@ -31,10 +31,10 @@ double kinetic_energy_ev(const kn::particle::ChargedSpecies<NX, 3>& p, size_t id
     return 0.5 * p.m() * (v.x * v.x + v.y * v.y + v.z * v.z) / kn::constants::e;
 }
 
-double collision_frequency(double neutral_density,
-                           double cross_section,
-                           double kinetic_energy,
-                           double mass) {
+double collision_frequency(const double neutral_density,
+                           const double cross_section,
+                           const double kinetic_energy,
+                           const double mass) {
     return neutral_density * cross_section *
            std::sqrt(2.0 * kn::constants::e * kinetic_energy / mass);
 }
@@ -43,27 +43,27 @@ double calc_p_null(double nu_prime, double dt) {
     return 1.0 - std::exp(-nu_prime * dt);
 }
 
-double interpolate_cross_section(const CrossSection& cs, double energy) {
+double interpolate_cross_section(const CrossSection& cs, const double energy) {
     if (energy <= cs.energy.front())
         return cs.cross_section.front();
-    else if (energy >= cs.energy.back())
+
+    if (energy >= cs.energy.back())
         return cs.cross_section.back();
-    else {
-        auto it = std::lower_bound(cs.energy.begin(), cs.energy.end(), energy);
 
-        size_t rhs = it - cs.energy.begin();
+    const auto it = std::lower_bound(cs.energy.begin(), cs.energy.end(), energy);
 
-        double x0 = cs.energy[rhs - 1];
-        double x1 = cs.energy[rhs];
-        double y0 = cs.cross_section[rhs - 1];
-        double y1 = cs.cross_section[rhs];
+    const size_t rhs = it - cs.energy.begin();
 
-        return y0 + (energy - x0) * (y1 - y0) / (x1 - x0);
-    }
+    const double x0 = cs.energy[rhs - 1];
+    const double x1 = cs.energy[rhs];
+    const double y0 = cs.cross_section[rhs - 1];
+    const double y1 = cs.cross_section[rhs];
+
+    return y0 + (energy - x0) * (y1 - y0) / (x1 - x0);
 }
 
 template <unsigned NX, unsigned NV>
-double total_cs(double energy, const Reactions<NX, NV>& reactions) {
+double total_cs(const double energy, const Reactions<NX, NV>& reactions) {
     double cs = 0.0;
     for (const auto& reaction : reactions)
         cs += interpolate_cross_section(reaction->m_cross_section, energy);
@@ -73,15 +73,14 @@ double total_cs(double energy, const Reactions<NX, NV>& reactions) {
 template <unsigned NX, unsigned NV>
 double max_sigmav_for_cross_section(const CrossSection& cs,
                                     const Reactions<NX, NV>& reactions,
-                                    double projectile_mass,
-                                    bool slow_projectiles) {
+                                    const double projectile_mass,
+                                    const bool slow_projectiles) {
     double nu_prime = 0.0;
     const double rmc = (slow_projectiles ? 2.0 : 1.0) * kn::constants::e / projectile_mass;
 
-    for (size_t i = 0; i < cs.energy.size(); i++) {
-        double energy = cs.energy[i];
-        double tcs = total_cs<NX>(energy, reactions);
-        double nu = tcs * std::sqrt(2.0 * energy * rmc);
+    for (double energy : cs.energy) {
+        const double tcs = total_cs<NX>(energy, reactions);
+        const double nu = tcs * std::sqrt(2.0 * energy * rmc);
         nu_prime = std::max(nu_prime, nu);
     }
 
@@ -106,16 +105,16 @@ MCCReactionSet<NX, NV>::MCCReactionSet(particle::ChargedSpecies<NX, NV>& project
     : m_projectile(projectile), m_config(std::move(config)) {
     m_max_sigmav = max_sigmav(m_config.m_reactions, m_projectile,
                               m_config.m_dyn == RelativeDynamics::SlowProjectile);
-};
+}
 
 template <unsigned NX, unsigned NV>
 void MCCReactionSet<NX, NV>::react_all() {
     double nu_prime = m_config.m_target->dens_max() * m_max_sigmav;
     double p_null = calc_p_null(nu_prime, m_config.m_dt);
 
-    double n_null_f = p_null * (double)m_projectile.n();
-    size_t n_null = (size_t)std::floor(n_null_f);
-    n_null = (n_null_f - (double)n_null) > random::uniform() ? n_null + 1 : n_null;
+    const double n_null_f = p_null * static_cast<double>(m_projectile.n());
+    auto n_null = static_cast<size_t>(std::floor(n_null_f));
+    n_null = n_null_f - static_cast<double>(n_null) > random::uniform() ? n_null + 1 : n_null;
 
     core::Vec<3> vrand;
 
@@ -128,11 +127,11 @@ void MCCReactionSet<NX, NV>::react_all() {
         auto& vp = m_projectile.v()[p_idx];
 
         if (m_config.m_dyn == RelativeDynamics::SlowProjectile) {
-            double vth =
-                std::sqrt(kn::constants::kb * m_config.m_target->temperature() / m_projectile.m());
+            const double vth =
+                std::sqrt(constants::kb * m_config.m_target->temperature() / m_projectile.m());
 
-            vrand = {kn::random::normal() * vth, kn::random::normal() * vth,
-                     kn::random::normal() * vth};
+            vrand = {random::normal() * vth, random::normal() * vth,
+                     random::normal() * vth};
 
             vp.x -= vrand.x;
             vp.y -= vrand.y;
@@ -140,7 +139,7 @@ void MCCReactionSet<NX, NV>::react_all() {
         }
 
         double kinetic_energy = kinetic_energy_ev(m_projectile, p_idx);
-        double r1 = random::uniform();
+        const double r1 = random::uniform();
 
         double fr0 = 0.0;
         double fr1 = 0.0;
