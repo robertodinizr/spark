035d8f84e5ae2f2e65d125849c455f33b72c6692
Lui Habl <txaicalvoso@gmail.com>
2024-12-06 19:19:26 -0300

    Remove Eigen library

--- cmake/external.cmake
+++ cmake/external.cmake
@@ -1,18 +1,3 @@
 include(cmake/cpm.cmake)
 
-# Eigen
-
-CPMAddPackage(
-  NAME Eigen
-  VERSION 3.4.0
-  URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
-  # Eigen's CMakelists are not intended for library use
-  DOWNLOAD_ONLY YES
-)
-
-if(Eigen_ADDED)
-  add_library(Eigen INTERFACE IMPORTED)
-  target_include_directories(Eigen INTERFACE ${Eigen_SOURCE_DIR})
-endif()
-
 CPMAddPackage("gh:luihabl/parallel-hashmap#master")
--- src/electromagnetics/poisson.cpp
+++ src/electromagnetics/poisson.cpp
@@ -1,95 +1,10 @@
 #include "spark/electromagnetics/poisson.h"
 
-#include <Eigen/Sparse>
 #include <iostream>
-#include <memory>
 
 #include "spark/constants/constants.h"
 
 using namespace spark::electromagnetics;
-
-struct SymmetricPoissonSolver::SolverImpl {
-    size_t n = 0;
-    Eigen::SparseQR<Eigen::SparseMatrix<double>, Eigen::AMDOrdering<int>> solver;
-    Eigen::VectorXd b, x;
-    Eigen::SparseMatrix<double> emat;
-};
-
-SymmetricPoissonSolver::SymmetricPoissonSolver() = default;
-SymmetricPoissonSolver::~SymmetricPoissonSolver() = default;
-
-SymmetricPoissonSolver::SymmetricPoissonSolver(SymmetricPoissonSolver&& other) noexcept {
-    m_impl = std::move(other.m_impl);
-}
-
-SymmetricPoissonSolver& SymmetricPoissonSolver::operator=(SymmetricPoissonSolver&& other) noexcept {
-    m_impl = std::move(other.m_impl);
-    return *this;
-}
-
-SymmetricPoissonSolver::SymmetricPoissonSolver(size_t n, double dx) {
-    m_impl = std::make_unique<SolverImpl>();
-    m_impl->n = n;
-
-    auto mat = Eigen::SparseMatrix<double>(n, n);
-
-    std::vector<Eigen::Triplet<double>> triplets;
-    double dx2 = dx * dx;
-
-    m_impl->b = Eigen::VectorXd(n);
-
-    for (size_t i = 0; i < n; i++) {
-        triplets.push_back(Eigen::Triplet<double>(i, i, -2.0 / dx2));
-
-        if (i < n - 1) {
-            triplets.push_back(Eigen::Triplet<double>(i + 1, i, 1.0 / dx2));
-            triplets.push_back(Eigen::Triplet<double>(i, i + 1, 1.0 / dx2));
-        }
-    }
-
-    triplets.push_back(Eigen::Triplet<double>(n - 1, 0, 1.0 / dx2));
-    triplets.push_back(Eigen::Triplet<double>(0, n - 1, 1.0 / dx2));
-
-    mat.setFromTriplets(triplets.begin(), triplets.end());
-
-    m_impl->solver.compute(mat);
-    if (m_impl->solver.info() != Eigen::Success) {
-        std::cout << "Solver failed to decompose matrix" << std::endl;
-    }
-
-    m_impl->emat = Eigen::SparseMatrix<double>(n, n);
-    triplets.clear();
-
-    for (size_t i = 0; i < n - 1; i++) {
-        triplets.push_back(Eigen::Triplet<double>(i + 1, i, 1.0 / (2.0 * dx)));
-        triplets.push_back(Eigen::Triplet<double>(i, i + 1, -1.0 / (2.0 * dx)));
-    }
-
-    triplets.push_back(Eigen::Triplet<double>(n - 1, 0, 1.0 / (2.0 * dx)));
-    triplets.push_back(Eigen::Triplet<double>(0, n - 1, -1.0 / (2.0 * dx)));
-    m_impl->emat.setFromTriplets(triplets.begin(), triplets.end());
-}
-
-void SymmetricPoissonSolver::solve(const std::vector<double>& density, std::vector<double>& out) {
-    size_t s = m_impl->b.size();
-
-    for (size_t i = 0; i < s; i++) {
-        m_impl->b(i) = density[i];
-    }
-
-    m_impl->x = m_impl->solver.solve(m_impl->b);
-    if (m_impl->solver.info() != Eigen::Success) {
-        std::cout << "Solver failed to solve system" << std::endl;
-    }
-
-    out.assign(m_impl->x.begin(), m_impl->x.end());
-}
-
-void SymmetricPoissonSolver::grad(std::vector<double>& out) {
-    auto e = (-m_impl->emat * m_impl->x).eval();
-    out.assign(e.begin(), e.end());
-    out[0] = out.back();
-}
 
 DirichletPoissonSolver::DirichletPoissonSolver(size_t n, double dx) : m_n(n), m_dx(dx) {}
 
@@ -150,9 +65,9 @@ void DirichletPoissonSolver::efield_extrapolate(const double* phi, double* eout,
 }
 
 void spark::electromagnetics::charge_density(double particle_weight,
-                                          const spark::spatial::UniformGrid& ion_density,
-                                          const spark::spatial::UniformGrid& electron_density,
-                                          spark::spatial::UniformGrid& out) {
+                                             const spark::spatial::UniformGrid& ion_density,
+                                             const spark::spatial::UniformGrid& electron_density,
+                                             spark::spatial::UniformGrid& out) {
     auto& out_data = out.data();
     auto& ne = electron_density.data();
     auto& ni = ion_density.data();
