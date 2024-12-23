0d5271f70fb6c0f072ce8dd61ad93792f656fafd
Lui Habl <txaicalvoso@gmail.com>
2024-12-22 22:17:33 -0300

    Implement particle pusher in 2d

--- src/particle/pusher.cpp
+++ src/particle/pusher.cpp
@@ -29,3 +29,20 @@ void spark::particle::move_particles(spark::particle::ChargedSpecies<1, 3>& spec
         x[i].x += v[i].x * dt;
     }
 }
+
+template <>
+void spark::particle::move_particles(spark::particle::ChargedSpecies<2, 3>& species, double dt) {
+    size_t n = species.n();
+    auto* v = species.v();
+    auto* x = species.x();
+    auto* f = species.f();
+    double k = species.q() * dt / species.m();
+
+    for (size_t i = 0; i < n; i++) {
+        v[i].x += f[i].x * k;
+        v[i].y += f[i].y * k;
+
+        x[i].x += v[i].x * dt;
+        x[i].y += v[i].y * dt;
+    }
+}
