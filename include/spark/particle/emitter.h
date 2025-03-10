7ab7e06d0b7ba852a280a035f785086daaf58851
Lui Habl <txaicalvoso@gmail.com>
2025-03-09 21:20:20 -0300

    Add combined component emitter

--- include/spark/particle/emitter.h
+++ include/spark/particle/emitter.h
@@ -22,35 +22,42 @@ inline auto uniform(const double vmin, const double vmax) {
 inline auto single_value(const double val) {
     return [val]() { return val; };
 }
+
+inline double maxwellian_flux(const double std, const double drift) {
+    return std * sqrt(-2 * log(spark::random::uniform())) + drift;
+}
+
 }  // namespace distributions
 
 template <unsigned NX, unsigned NV>
 class Emitter {
 public:
+    Emitter(double rate) : rate_(rate) {}
+
     using Ref = std::unique_ptr<Emitter<NX, NV>>;
-    virtual void emit(Species<NX, NV>& species, int n) = 0;
+    virtual void emit(Species<NX, NV>& species, double n) = 0;
     void emit(Species<NX, NV>& species) { emit(species, rate_); }
-    void set_rate(int rate) { rate_ = rate; }
+    void set_rate(double rate) { rate_ = rate; }
     virtual ~Emitter() {}
 
 protected:
-    int rate_ = 0;
+    double rate_ = 0;
 };
 
 template <unsigned NX, unsigned NV, typename... Args>
     requires(sizeof...(Args) == NX + NV)
-class EmitterImpl : public Emitter<NX, NV> {
+class IndividualComponentEmitter : public Emitter<NX, NV> {
     using super = Emitter<NX, NV>;
 
 public:
-    EmitterImpl() = default;
-    EmitterImpl(int rate, Args&&... distributions)
-        : distributions_(std::make_tuple(std::forward<Args>(distributions)...)) {
-        Emitter<NX, NV>::rate_ = rate;
-    }
+    IndividualComponentEmitter(double rate, Args&&... distributions)
+        : super(rate), distributions_(std::make_tuple(std::forward<Args>(distributions)...)) {}
 
-    void emit(Species<NX, NV>& species, int n) override {
-        species.add(n, [this](auto& v, auto& x) {
+    void emit(Species<NX, NV>& species, double n) override {
+        double fn = floor(n);
+        int np = spark::random::uniform() <= (n - fn) ? fn + 1 : fn;
+
+        species.add(np, [this](auto& v, auto& x) {
             if constexpr (NX > 0)
                 x.x = std::get<0>(distributions_)();
             if constexpr (NX > 1)
@@ -71,15 +78,53 @@ private:
     std::tuple<Args...> distributions_;
 };
 
+template <unsigned NX, unsigned NV, typename F>
+class CombinedComponentEmitter : public Emitter<NX, NV> {
+    using super = Emitter<NX, NV>;
+
+public:
+    CombinedComponentEmitter(double rate, F&& distribution)
+        : super(rate), distribution_(std::forward<F>(distribution)) {}
+
+    void emit(Species<NX, NV>& species, double n) override {
+        double fn = floor(n);
+        int np = spark::random::uniform() <= (n - fn) ? fn + 1 : fn;
+
+        species.add(np, [this](auto& v, auto& x) {
+            const auto sample = distribution_();
+            x = std::get<0>(sample);
+            v = std::get<1>(sample);
+        });
+    }
+
+private:
+    F distribution_;
+};
+
 template <unsigned NX, unsigned NV, typename... Args>
-inline Emitter<NX, NV>::Ref make_emitter(Args&&... distributions) {
-    return std::make_unique<EmitterImpl<NX, NV, Args...>>(0, std::forward<Args>(distributions)...);
+    requires(sizeof...(Args) == NX + NV)
+inline Emitter<NX, NV>::Ref make_emitter(Args&&... individual_distributions) {
+    return std::make_unique<IndividualComponentEmitter<NX, NV, Args...>>(
+        0, std::forward<Args>(individual_distributions)...);
 }
 
 template <unsigned NX, unsigned NV, typename... Args>
-inline Emitter<NX, NV>::Ref make_emitter(int rate, Args&&... distributions) {
-    return std::make_unique<EmitterImpl<NX, NV, Args...>>(rate,
-                                                          std::forward<Args>(distributions)...);
+    requires(sizeof...(Args) == NX + NV)
+inline Emitter<NX, NV>::Ref make_emitter(double rate, Args&&... individual_distributions) {
+    return std::make_unique<IndividualComponentEmitter<NX, NV, Args...>>(
+        rate, std::forward<Args>(individual_distributions)...);
+}
+
+template <unsigned NX, unsigned NV, typename F>
+inline Emitter<NX, NV>::Ref make_emitter(F&& combined_distribution) {
+    return std::make_unique<CombinedComponentEmitter<NX, NV, F>>(
+        0, std::forward<F>(combined_distribution));
+}
+
+template <unsigned NX, unsigned NV, typename F>
+inline Emitter<NX, NV>::Ref make_emitter(double rate, F&& combined_distribution) {
+    return std::make_unique<CombinedComponentEmitter<NX, NV, F>>(
+        rate, std::forward<F>(combined_distribution));
 }
 
 }  // namespace spark::particle
