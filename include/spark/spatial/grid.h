332b5e4bb89f4839c77dc45ec2f2dd6aec4151bf
Thamiris T Libard <42550287+robertodinizr@users.noreply.github.com>
2025-05-05 02:01:55 -0300

    Adjust functions

--- include/spark/spatial/grid.h
+++ include/spark/spatial/grid.h
@@ -51,6 +51,37 @@ public:
         }
     }
 
+    T get_radius(size_t i_r) const {
+        return static_cast<T>(i_r) * prop_.dx.y;
+    }
+
+    T get_mid_radius(size_t i_r) const {
+        return (static_cast<T>(i_r) + static_cast<T>(0.5)) * prop_.dx.y;
+    }
+
+    T get_annular_cell_volume(size_t i_r, size_t i_z) const {
+        const T dr = prop_.dx.y;
+        const T dz = prop_.dx.x;
+        T r_outer, r_inner;
+
+        r_outer = get_mid_radius(i_r);
+
+        if (i_r == 0) {
+            r_inner = static_cast<T>(0.0);
+        } else {
+            r_inner = get_mid_radius(i_r - 1);
+        }
+
+        return constants::pi * (r_outer * r_outer - r_inner * r_inner) * dz;
+    }
+
+    void get_cell_area(size_t i_r, size_t i_z, T& area) const {
+        T dr = prop_.dx.y;
+        T dz = prop_.dx.x;
+        T r_center = (static_cast<T>(i_r) + static_cast<T>(0.5)) * dr;
+        area = r_center * dr * dz;
+    }
+
 private:
     GridProp<N> prop_;
     core::TMatrix<T, N> data_;
@@ -58,70 +89,6 @@ private:
 
 template <unsigned N>
 using UniformGrid = TUniformGrid<double, N>;
-
-template <typename T>
-class TCylindricalGrid {
-public:
-    TCylindricalGrid() = default;
-
-    TCylindricalGrid(const core::Vec<2>& l, const core::ULongVec<2>& n) {
-        prop_.l = l;
-        prop_.n = n;
-        prop_.dx = l / (n.template to<double>() - 1.0);
-        data_.resize(n);
-        set({0});
-    }
-
-    TCylindricalGrid(const GridProp<2>& prop) : prop_(prop) {
-        data_.resize(prop_.n);
-        set({0});
-    }
-
-    void set(T v) { data_.fill(v); }
-    auto& data() { return data_; }
-    const auto& data() const { return data_; }
-    auto* data_ptr() const { return const_cast<T*>(data_.data_ptr()); }
-    auto n_total() const { return data_.count(); }
-    auto n() const { return data_.size(); }
-    auto l() const { return prop_.l; }
-    auto dx() const { return prop_.dx; }
-    auto prop() const { return prop_; }
-
-    T getRadius(size_t i_r) const {
-        return static_cast<T>(i_r) * prop_.dx.x;
-    }
-
-    T getMidRadius(size_t i_r) const {
-        return (static_cast<T>(i_r) + static_cast<T>(0.5)) * prop_.dx.x;
-    }
-
-    T getCellVolume(size_t i_r, size_t i_z) const {
-        T dz = prop_.dx.y;
-        T r_outer = getMidRadius(i_r); 
-        T r_inner;
-
-        if (i_r == 0) {
-            r_inner = static_cast<T>(0.0);
-        } else {
-            r_inner = getMidRadius(i_r - 1);
-        }
-
-        return constants::pi * (r_outer * r_outer - r_inner * r_inner) * dz;
-    }
-
-    T cellArea(size_t i_r, size_t i_z) const {
-        T dr = prop_.dx.x;
-        T dz = prop_.dx.y;
-        T r_center = (static_cast<T>(i_r) + static_cast<T>(0.5)) * dr;
-        return r_center * dr * dz;
-    }
-
-private:
-    GridProp<2> prop_;
-    core::TMatrix<T, 2> data_;
-};
-
-using CylindricalGrid = TCylindricalGrid<double>;
 
 template <unsigned N>
 class AverageGrid {
