b19956ad03adf2c61ad36ac90e52736aa12fcdc9
Lui Habl <txaicalvoso@gmail.com>
2025-07-18 10:13:13 -0300

    Add other iodine reactions

--- include/spark/collisions/basic_reactions.h
+++ include/spark/collisions/basic_reactions.h
@@ -131,6 +131,37 @@ public:
 };
 
 template <unsigned NX, unsigned NV>
+class IonDiffMassesElasticCollision final : public BasicCollision<NX, NV> {
+public:
+    using BasicCollision<NX, NV>::BasicCollision;
+
+    IonDiffMassesElasticCollision(double projectile_mass, double target_mass, CrossSection&& cs)
+        : BasicCollision<NX, NV>(BasicCollisionConfig{}, std::forward<CrossSection>(cs)) {
+        gamma_ = projectile_mass / target_mass;
+    }
+
+    ReactionOutcome react(particle::ChargedSpecies<NX, NV>& projectile,
+                          size_t id,
+                          double kinetic_energy) override {
+        const double r = random::uniform();
+        const double chi = atan(sqrt(r * (1 - r)) / ((gamma_ + 1) / 2 - r));
+        const double cc = cos(chi);
+        double efactor = ((cc + sqrt(cc * cc + 3)) / 3);
+        efactor *= efactor;
+
+        const double v_mag =
+            std::sqrt(2.0 * constants::e * (kinetic_energy * efactor) / projectile.m());
+
+        scattering::isotropic_coll(projectile, id, v_mag, chi);
+
+        return ReactionOutcome::Collided;
+    }
+
+private:
+    double gamma_ = 0;
+};
+
+template <unsigned NX, unsigned NV>
 class ChargeExchangeCollision final : public BasicCollision<NX, NV> {
 public:
     using BasicCollision<NX, NV>::BasicCollision;
@@ -138,9 +169,42 @@ public:
     ReactionOutcome react(particle::ChargedSpecies<NX, NV>& projectile,
                           size_t id,
                           double kinetic_energy) override {
+        // Zero since this is going to go back to the target ref frame
         projectile.v()[id] = core::Vec<3>();
         return ReactionOutcome::Collided;
     }
+};
+
+template <unsigned NX, unsigned NV>
+class ChargeExchangeDiffCollision final : public BasicCollision<NX, NV> {
+public:
+    ChargeExchangeDiffCollision(particle::ChargedSpecies<NX, NV>* target_species,
+                                const double t_neutral,
+                                BasicCollisionConfig config,
+                                CrossSection&& cs)
+        : BasicCollision<NX, NV>(config, std::forward<CrossSection>(cs)),
+          target_species_(target_species),
+          t_neutral_(t_neutral) {};
+
+    ReactionOutcome react(particle::ChargedSpecies<NX, NV>& projectile,
+                          size_t id,
+                          double kinetic_energy) override {
+        auto event_pos = projectile.x()[id];
+        double ion_temp = t_neutral_;
+        double ion_mass = target_species_->m();
+
+        target_species_->add(1, [event_pos, ion_mass, ion_temp](core::Vec<3>& v, core::Vec<NX>& x) {
+            x = event_pos;
+            const double v_th = std::sqrt(spark::constants::kb * ion_temp / ion_mass);
+            v = {random::normal(0.0, v_th), random::normal(0.0, v_th), random::normal(0.0, v_th)};
+        });
+
+        return ReactionOutcome::Collided | ReactionOutcome::ProjectileToBeRemoved;
+    }
+
+private:
+    spark::particle::ChargedSpecies<NX, NV>* target_species_ = nullptr;
+    double t_neutral_ = 0.0;
 };
 
 }  // namespace spark::collisions::reactions
